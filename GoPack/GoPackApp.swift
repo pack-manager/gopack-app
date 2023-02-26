6f6080ce218890bc927cfeaa7feb3dd9112b5b80
Matheus Evangelista <matheus.evangelista@g.globo>
2023-02-25 22:17:36 -0300

    Initial commit

---
+++ GoPack/GoPackApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoPackApp.swift
+//  GoPack
+//
+//  Created by Matheus Evangelista on 25/02/23.
+//
+
+import SwiftUI
+
+@main
+struct GoPackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
